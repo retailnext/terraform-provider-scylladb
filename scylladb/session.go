ab2b4e0f46ea7e0236403d847fd0c7d3a867662e
Byungsoo Kim <7953203+i1snow@users.noreply.github.com>
2026-02-06 09:03:50 -0500

    utilize proxy if exists

--- scylladb/session.go
+++ scylladb/session.go
@@ -3,9 +3,19 @@
 package scylladb
 
 import (
+	"bufio"
+	"cmp"
+	"context"
 	"crypto/tls"
 	"crypto/x509"
 	"errors"
+	"fmt"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
 
 	gocql "github.com/apache/cassandra-gocql-driver/v2"
 )
@@ -16,8 +26,70 @@ type Cluster struct {
 	Session                *gocql.Session
 }
 
+type ProxyDialer struct {
+	ProxyURL *url.URL
+}
+
+type BufferedConn struct {
+	net.Conn
+	r *bufio.Reader
+}
+
+func (b *BufferedConn) Read(p []byte) (int, error) {
+	return b.r.Read(p)
+}
+
+func (p *ProxyDialer) DialContext(ctx context.Context, network, addr string) (net.Conn, error) {
+	// 1. Dial the proxy server itself
+	if _, ok := ctx.Deadline(); !ok {
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
+		defer cancel()
+	}
+	var d net.Dialer
+	proxyConn, err := d.DialContext(ctx, "tcp", p.ProxyURL.Host)
+	if err != nil {
+		return nil, err
+	}
+	// 2. Request a tunnel to the Cassandra node. addr ~ 1.2.3.4:9042
+	req := fmt.Sprintf("CONNECT %s HTTP/1.1\r\nHost: %s\r\n\r\n", addr, addr)
+	if _, err := proxyConn.Write([]byte(req)); err != nil {
+		proxyConn.Close()
+		return nil, err
+	}
+
+	// 3. Read the response from Tinyproxy
+	br := bufio.NewReader(proxyConn)
+	resp, err := http.ReadResponse(br, &http.Request{Method: http.MethodConnect})
+	if err != nil {
+		proxyConn.Close()
+		return nil, fmt.Errorf("failed to read proxy response: %w", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		proxyConn.Close()
+		return nil, fmt.Errorf("proxy refused connection: %s", resp.Status)
+	}
+
+	if br.Buffered() > 0 {
+		return &BufferedConn{Conn: proxyConn, r: br}, nil
+	}
+
+	// 4. Return the hijacked connection to gocql
+	return proxyConn, nil
+}
+
 func NewClusterConfig(hosts []string) Cluster {
 	cluster := gocql.NewCluster(hosts...)
+	// Check proxy
+	proxyStr := cmp.Or(os.Getenv("HTTPS_PROXY"), os.Getenv("HTTP_PROXY"))
+	if proxyStr != "" {
+		if !strings.HasPrefix(proxyStr, "http://") && !strings.HasPrefix(proxyStr, "https://") {
+			proxyStr = "http://" + proxyStr
+		}
+		proxyURL, _ := url.Parse(proxyStr)
+		cluster.Dialer = &ProxyDialer{ProxyURL: proxyURL}
+	}
 	cluster.DisableInitialHostLookup = true
 	cluster.NumConns = 1
 	return Cluster{
@@ -63,6 +135,9 @@ func (c *Cluster) SetTLS(caCert, clientCert, clientKey []byte, enableHostVerific
 			return err
 		}
 		tlsConfig.Certificates = []tls.Certificate{cert}
+		tlsConfig.GetClientCertificate = func(*tls.CertificateRequestInfo) (*tls.Certificate, error) {
+			return &cert, error(nil)
+		}
 	}
 	c.Cluster.SslOpts = &gocql.SslOptions{
 		Config: tlsConfig,
